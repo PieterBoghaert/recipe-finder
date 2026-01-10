a27b965d71c9f78eae6fe0509508c51fd06e90c0
Pieter Boghaert <pieter.boghaert@gmail.com>
2026-01-10 22:50:38 +0100

    Add meta tags for description and author in the layout

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,7 +5,11 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="description" content="Discover quick, whole-food recipes that anyone can master—no fancy equipment, no ultra-processed shortcuts—just honest ingredients and straightforward steps.">
+    <meta name="author" content="Boghaert Pieter">
+    <link rel="icon" type="image/png" href="{{ asset('assets/images/favicon-32x32.png') }}">
 
+    {{-- Page Title --}}
     <title>{{ $title ?? 'Recipe Finder' }}</title>
 
     <!-- Fonts -->
